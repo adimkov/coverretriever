31e526e0856d422e6cbefcaaa662a3f147476787
Anton Dimkov <ddt.sterh@gmail.com>
2011-09-16 00:06:49 +0300

    Fixed obtain cover from Tag Signed-off-by: Anton Dimkov <ddt.sterh@gmail.com> Fixed defect with obtain cover from Tag file. Defect occured when ITunes saves cover in Tag without correct picture type (255 instead of FrontCover)

--- CoverRetriever.AudioInfo/AudioFileMetaProvider.cs
+++ CoverRetriever.AudioInfo/AudioFileMetaProvider.cs
@@ -1,201 +1,240 @@
-
-using System;
-using System.IO;
-using System.Linq;
-using CoverRetriever.AudioInfo.Helper;
-using TagLib;
-using File = TagLib.File;
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AudioFileMetaProvider.cs" author="Anton Dimkov">
+//   Copyright (c) Anton Dimkov 2011. All rights reserved.  
+// </copyright>
+// <summary>
+//  Base class to obtain Meta info from file name.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace CoverRetriever.AudioInfo
 {
-	/// <summary>
-	/// Obtain Meta info from file name
-	/// </summary>
-	public abstract class AudioFileMetaProvider : IMetaProvider, IActivator, ICoverOrganizer, IDisposable
-	{
-		private File _file;
-		private bool _initialized;
-		private bool _disposed;
+    using System;
+    using System.IO;
+    using System.Linq;
 
-		public AudioFileMetaProvider()
-		{
-			FileNameMetaObtainer = new FileNameMetaObtainer();
-		}
+    using CoverRetriever.AudioInfo.Helper;
 
-		/// <summary>
-		/// Indicate is Meta Data empty
-		/// </summary>
-		public virtual bool IsEmpty
-		{
-			get
-			{
-				EnsureInstanceWasNotDisposed();
-				return _file.Tag.IsEmpty;
-			}
-		}
+    using TagLib;
 
-		/// <summary>
-		/// Indicate if can work with cover
-		/// </summary>
-		public virtual bool IsCanProcessed
-		{
-			get
-			{
-				EnsureInstanceWasNotDisposed();
-				return true;
-			}
-		}
+    using File = TagLib.File;
 
-		/// <summary>
-		/// Get FileNameMetaObtainer 
-		/// </summary>
-		protected FileNameMetaObtainer FileNameMetaObtainer { get; private set; }
+    /// <summary>
+    /// Base class to obtain Meta info from file name.
+    /// </summary>
+    public abstract class AudioFileMetaProvider : IMetaProvider, IActivator, ICoverOrganizer, IDisposable
+    {
+        /// <summary>
+        /// Audio file.
+        /// </summary>
+        private File _file;
 
-		/// <summary>
-		/// Get tag file
-		/// </summary>
-		protected File File
-		{
-			get { return _file; }
-		}
+        /// <summary>
+        /// The flag indicating is audio file initialized.
+        /// </summary>
+        private bool _initialized;
 
-		/// <summary>
-		/// Get an album name
-		/// </summary>
-		/// <returns></returns>
-		public virtual string GetAlbum()
-		{
-			EnsureInstanceWasNotDisposed();
-			return _file.Tag.Album ?? FileNameMetaObtainer.GetAlbum();
-		}
+        /// <summary>
+        /// The flag indicating is audio file disposed.
+        /// </summary>
+        private bool _disposed;
 
-		/// <summary>
-		/// Get an artist
-		/// </summary>
-		/// <returns></returns>
-		public virtual string GetArtist()
-		{
-			EnsureInstanceWasNotDisposed();
-			return _file.Tag.FirstArtist ?? FileNameMetaObtainer.GetArtist();
-		}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AudioFileMetaProvider"/> class.
+        /// </summary>
+        protected AudioFileMetaProvider()
+        {
+            FileNameMetaObtainer = new FileNameMetaObtainer();
+        }
 
-		/// <summary>
-		/// Get year of album
-		/// </summary>
-		/// <returns></returns>
-		public virtual string GetYear()
-		{
-			EnsureInstanceWasNotDisposed();
-			return _file.Tag.Year >= 0 ? _file.Tag.Year.ToString() : FileNameMetaObtainer.GetYear();
-		}
+        /// <summary>
+        /// Gets a value indicating whether Tag empty.
+        /// </summary>
+        public virtual bool IsEmpty
+        {
+            get
+            {
+                EnsureInstanceWasNotDisposed();
+                return _file.Tag.IsEmpty;
+            }
+        }
 
-		/// <summary>
-		/// Get name  of track
-		/// </summary>
-		/// <returns></returns>
-		public virtual string GetTrackName()
-		{
-			EnsureInstanceWasNotDisposed();
-			return _file.Tag.Title ?? FileNameMetaObtainer.GetTrackName();
-		}
+        /// <summary>
+        /// Gets a value indicating whether can perform operations with Cover.
+        /// </summary>
+        public virtual bool IsCanProcessed
+        {
+            get
+            {
+                EnsureInstanceWasNotDisposed();
+                return true;
+            }
+        }
 
-		
-		/// <summary>
-		/// Indicate the cover existence
-		/// </summary>
-		/// <returns><see cref="True"/> if cover exists</returns>
-		public virtual bool IsCoverExists()
-		{
-			EnsureInstanceWasNotDisposed();
-			return _file.Tag.GetCoverSafe(PictureType.FrontCover) != null;
-		}
+        /// <summary>
+        /// Gets service to obtain name of file. 
+        /// </summary>
+        protected FileNameMetaObtainer FileNameMetaObtainer { get; private set; }
 
-		/// <summary>
-		/// Get cover
-		/// </summary>
-		/// <returns>Cover info</returns>
-		public virtual Cover GetCover()
-		{
-			EnsureInstanceWasNotDisposed();
-			return _file.Tag.GetCoverSafe(PictureType.FrontCover).PrepareCover();
-		}
+        /// <summary>
+        /// Gets audio file.
+        /// </summary>
+        protected File File
+        {
+            get { return _file; }
+        }
 
-		/// <summary>
-		/// Save stream into cover
-		/// </summary>
-		/// <param name="cover">Cover to save</param>
-		public virtual IObservable<Unit> SaveCover(Cover cover)
-		{
-			EnsureInstanceWasNotDisposed();
-			var saveResult = cover.CoverStream
-				.Do(
-					stream =>
-					{
-						_file.Tag.ReplacePictures(PictureHelper.PreparePicture(stream, cover.Name, PictureType.FrontCover));
-						_file.Save();
-					})
-				.Select(x => new Unit())
-				.Take(1);
+        /// <summary>
+        /// Gets a name of album.
+        /// </summary>
+        /// <returns>Album name.</returns>
+        public virtual string GetAlbum()
+        {
+            EnsureInstanceWasNotDisposed();
+            return _file.Tag.Album ?? FileNameMetaObtainer.GetAlbum();
+        }
 
-			return saveResult;
-		}
+        /// <summary>
+        /// Gets an artist.
+        /// </summary>
+        /// <returns>The artist.</returns>
+        public virtual string GetArtist()
+        {
+            EnsureInstanceWasNotDisposed();
+            return _file.Tag.FirstPerformer ?? FileNameMetaObtainer.GetArtist();
+        }
 
-		/// <summary>
-		/// Activate an object
-		/// </summary>
-		/// <param name="param"></param>
-		public void Activate(params object[] param)
-		{
-			if (!_initialized)
-			{
-				var filePath = (string)param[0];
-				_file = GetTagFile(filePath);
-				FileNameMetaObtainer.ParseFileName(Path.GetFileNameWithoutExtension(filePath));
-				_initialized = true;
-			}
-			else
-			{
-				throw new MetaProviderException("Instance already has been initialized");
-			}
-		}
+        /// <summary>
+        /// Gets year of album.
+        /// </summary>
+        /// <returns>Year of album.</returns>
+        public virtual string GetYear()
+        {
+            EnsureInstanceWasNotDisposed();
+            return _file.Tag.Year >= 0 ? _file.Tag.Year.ToString() : FileNameMetaObtainer.GetYear();
+        }
 
-		/// <summary>
-		/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-		/// </summary>
-		/// <filterpriority>2</filterpriority>
-		public virtual void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
+        /// <summary>
+        /// Get name of track.
+        /// </summary>
+        /// <returns>Track name.</returns>
+        public virtual string GetTrackName()
+        {
+            EnsureInstanceWasNotDisposed();
+            return _file.Tag.Title ?? FileNameMetaObtainer.GetTrackName();
+        }
+        
+        /// <summary>
+        /// Gets a value indicating whether the cover existence.
+        /// </summary>
+        /// <returns><c>true</c> if cover exists; otherwise <c>false</c></returns>
+        public virtual bool IsCoverExists()
+        {
+            EnsureInstanceWasNotDisposed();
+            return _file.Tag.GetCoverSafe(PictureType.FrontCover) != null;
+        }
 
-		protected virtual void Dispose(bool disposing)
-		{
-			if (!_disposed)
-			{
-				if (disposing)
-				{
-					if (_file != null)
-					{
-						_file.Dispose();
-					}
-				}
-				_file = null;
-				_disposed = true;
-			}
-		}
+        /// <summary>
+        /// Gets the <see cref="Cover"/> from Tag.
+        /// </summary>
+        /// <returns>The <see cref="Cover"/>.</returns>
+        public virtual Cover GetCover()
+        {
+            EnsureInstanceWasNotDisposed();
+            return _file.Tag.GetCoverSafe(PictureType.FrontCover).PrepareCover();
+        }
 
-		protected virtual File GetTagFile(string fileName)
-		{
-			return File.Create(fileName, ReadStyle.None);
-		}
+        /// <summary>
+        /// Saves <see cref="Cover"/> in to Tag.
+        /// </summary>
+        /// <param name="cover"><see cref="Cover"/> to save.</param>
+        /// <returns>Operation result observer.</returns>
+        public virtual IObservable<Unit> SaveCover(Cover cover)
+        {
+            EnsureInstanceWasNotDisposed();
+            var saveResult = cover.CoverStream
+                .Do(
+                    stream =>
+                    {
+                        _file.Tag.ReplacePictures(PictureHelper.PreparePicture(stream, cover.Name, PictureType.FrontCover));
+                        _file.Save();
+                    })
+                .Select(x => new Unit())
+                .Take(1);
 
-		protected void EnsureInstanceWasNotDisposed()
-		{
-			if (_disposed)
-			{
-				throw new ObjectDisposedException("Meta provider was disposed");
-			}
-		}
-	}
+            return saveResult;
+        }
+
+        /// <summary>
+        /// Activates an audio file.
+        /// </summary>
+        /// <param name="param">Parameters to activate audio file.</param>
+        public void Activate(params object[] param)
+        {
+            if (!_initialized)
+            {
+                var filePath = (string)param[0];
+                _file = GetTagFile(filePath);
+                FileNameMetaObtainer.ParseFileName(Path.GetFileNameWithoutExtension(filePath));
+                _initialized = true;
+            }
+            else
+            {
+                throw new MetaProviderException("Instance already has been initialized");
+            }
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        /// <filterpriority>2</filterpriority>
+        public virtual void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    if (_file != null)
+                    {
+                        _file.Dispose();
+                    }
+                }
+
+                _file = null;
+                _disposed = true;
+            }
+        }
+
+        /// <summary>
+        /// Gets the tag file.
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        /// <returns>The <see cref="File"/>.</returns>
+        protected virtual File GetTagFile(string fileName)
+        {
+            return File.Create(fileName, ReadStyle.None);
+        }
+
+        /// <summary>
+        /// Ensures the instance was not disposed.
+        /// </summary>
+        /// <exception cref="ObjectDisposedException">If file was disposed.</exception>
+        protected void EnsureInstanceWasNotDisposed()
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException("Meta provider was disposed");
+            }
+        }
+    }
 }
--- CoverRetriever.AudioInfo/Helper/PictureHelper.cs
+++ CoverRetriever.AudioInfo/Helper/PictureHelper.cs
@@ -1,126 +1,151 @@
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using TagLib;
-using Size = System.Windows.Size;
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PictureHelper.cs" author="Anton Dimkov">
+//   Copyright (c) Anton Dimkov 2011. All rights reserved.  
+// </copyright>
+// <summary>
+//  Utility class to work with pictures in the tag
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace CoverRetriever.AudioInfo.Helper
 {
-	/// <summary>
-	/// Utility class provide helper methods to FramePicture
-	/// </summary>
-	public static class PictureHelper
-	{
-		private static Dictionary<string, string> _mimeTipeDictionary = new Dictionary<string, string>
-		{
-			{".jpeg", "image/jpeg"},
-			{".jpg", "image/jpeg"},
-			{".jpe", "image/jpeg"},
-			{".png", "image/png"},
-			{".bmp", "image/bmp"},
-			{".gif", "image/gif"},
-		};
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.IO;
+    using System.Linq;
 
-		/// <summary>
-		/// Get MimeType by file extension
-		/// </summary>
-		/// <param name="fileExtension"></param>
-		/// <returns></returns>
-		public static string GetMimeTipeFromFileExtension(string fileExtension)
-		{
-			if (_mimeTipeDictionary.ContainsKey(fileExtension))
-			{
-				return _mimeTipeDictionary[fileExtension];
-			}
-			throw new MetaProviderException("MimeType for extension '{0}' was not found");
-		}
+    using TagLib;
 
-		/// <summary>
-		/// Replace pictures in Tag file
-		/// </summary>
-		/// <param name="tag">Mate tag</param>
-		/// <param name="newPictures">new pictures</param>
-		public static void ReplacePictures(this Tag tag, params IPicture[] newPictures)
-		{
-			var pictures = new List<IPicture>(tag.Pictures);
-			
-			foreach (var picture in newPictures)
-			{
-				var localPicture = picture;
-				int indexOfPicture = pictures.IndexOf(pictures.SingleOrDefault(x => x.Type == localPicture.Type));
-				
-				if (indexOfPicture >= 0)
-				{
-					pictures.RemoveAt(indexOfPicture);
-					pictures.Insert(indexOfPicture, localPicture);
-				}
-				else
-				{
-					pictures.Add(localPicture);
-				}
-			}
+    using Size = System.Windows.Size;
 
-			tag.Pictures = pictures.ToArray();
-		}
+    /// <summary>
+    /// Utility class to work with pictures in the tag.
+    /// </summary>
+    public static class PictureHelper
+    {
+        /// <summary>
+        /// Mime type to file extensions mapper.
+        /// </summary>
+        private static readonly Dictionary<string, string> MimeTipeDictionary = new Dictionary<string, string>
+            {
+                { ".jpeg", "image/jpeg" },
+                { ".jpg", "image/jpeg" },
+                { ".jpe", "image/jpeg" },
+                { ".png", "image/png" },
+                { ".bmp", "image/bmp" },
+                { ".gif", "image/gif" },
+            };
 
-		/// <summary>
-		/// Prepare front cover for inject into audio file 
-		/// </summary>
-		/// <param name="coverStream">stream of cover</param>
-		/// <param name="name">name of cover</param>
-		/// <returns></returns>
-		public static IPicture PreparePicture(Stream coverStream, string name, PictureType pictureType)
-		{
-			var buffer = new byte[1024 * 10];
-			var read = 0;
-			var downloadedCover = new MemoryStream();
+        /// <summary>
+        /// Get MimeType by file extension.
+        /// </summary>
+        /// <param name="fileExtension">Extension of file.</param>
+        /// <returns>The Mime type.</returns>
+        public static string GetMimeTipeFromFileExtension(string fileExtension)
+        {
+            if (MimeTipeDictionary.ContainsKey(fileExtension))
+            {
+                return MimeTipeDictionary[fileExtension];
+            }
 
-			do
-			{
-				read = coverStream.Read(buffer, 0, buffer.Length);
-				downloadedCover.Write(buffer, 0, read);
-			} while (read != 0);
-			
-			var frontCover = new Picture(new ByteVector(downloadedCover.ToArray(), (int)downloadedCover.Length));
-			frontCover.Type = pictureType;
-			frontCover.MimeType = GetMimeTipeFromFileExtension(Path.GetExtension(name));
+            throw new MetaProviderException("MimeType for extension '{0}' was not found");
+        }
 
-			return frontCover;
-		}
+        /// <summary>
+        /// Replace pictures in Tag file.
+        /// </summary>
+        /// <param name="tag">Audio tag.</param>
+        /// <param name="newPictures">The new pictures.</param>
+        public static void ReplacePictures(this Tag tag, params IPicture[] newPictures)
+        {
+            var pictures = new List<IPicture>(tag.Pictures);
+            
+            foreach (var picture in newPictures)
+            {
+                var localPicture = picture;
+                int indexOfPicture = pictures.IndexOf(tag.GetCoverSafe(localPicture.Type));
+                
+                if (indexOfPicture >= 0)
+                {
+                    pictures.RemoveAt(indexOfPicture);
+                    pictures.Insert(indexOfPicture, localPicture);
+                }
+                else
+                {
+                    pictures.Add(localPicture);
+                }
+            }
 
-		/// <summary>
-		/// Prepare cover from picture of tag
-		/// </summary>
-		/// <param name="picture">Picture from tag</param>
-		/// <returns></returns>
-		public static Cover PrepareCover(this IPicture picture)
-		{
-			var name = String.Format("{0}.{1}", picture.Type, picture.MimeType.Split('/')[1]);
-			var size = Size.Empty;
-			var coverStream = new MemoryStream(picture.Data.Count);
+            tag.Pictures = pictures.ToArray();
+        }
 
-			coverStream.Write(picture.Data.Data, 0, picture.Data.Count);
-			coverStream.Flush();
-			coverStream.Position = 0;
+        /// <summary>
+        /// Prepare front cover for inject into audio file. 
+        /// </summary>
+        /// <param name="coverStream">Stream of cover.</param>
+        /// <param name="name">Name of cover.</param>
+        /// <param name="pictureType">Type of picture.</param>
+        /// <returns>The <see cref="Picture"/>.</returns>
+        public static IPicture PreparePicture(Stream coverStream, string name, PictureType pictureType)
+        {
+            var buffer = new byte[1024 * 10];
+            int read;
+            var downloadedCover = new MemoryStream();
 
-			using (var bitmap = new Bitmap(coverStream))
-			{
-				size = new Size(bitmap.Width, bitmap.Height);
-			}
-			coverStream.Position = 0;
-			var cover = new Cover(name, size, coverStream.Length, Observable.Return(coverStream));
-			return cover;	
-		}
+            do
+            {
+                read = coverStream.Read(buffer, 0, buffer.Length);
+                downloadedCover.Write(buffer, 0, read);
+            } 
+            while (read != 0);
+            
+            var frontCover = new Picture(new ByteVector(downloadedCover.ToArray(), (int)downloadedCover.Length));
+            frontCover.Type = pictureType;
+            frontCover.MimeType = GetMimeTipeFromFileExtension(Path.GetExtension(name));
 
-		/// <summary>
-		/// Get cover from file if exists
-		/// </summary>
-		/// <returns></returns>
-		public static IPicture GetCoverSafe(this Tag tag, PictureType pictureType)
-		{
-			return tag.Pictures.SingleOrDefault(x => x.Type == pictureType);
-		}
-	}
+            return frontCover;
+        }
+
+        /// <summary>
+        /// Prepare cover from picture in the tag.
+        /// </summary>
+        /// <param name="picture">Picture from tag.</param>
+        /// <returns>The <see cref="Cover"/>.</returns>
+        public static Cover PrepareCover(this IPicture picture)
+        {
+            var name = string.Format("{0}.{1}", picture.Type, picture.MimeType.Split('/')[1]);
+            Size size;
+            var coverStream = new MemoryStream(picture.Data.Count);
+
+            coverStream.Write(picture.Data.Data, 0, picture.Data.Count);
+            coverStream.Flush();
+            coverStream.Position = 0;
+
+            using (var bitmap = new Bitmap(coverStream))
+            {
+                size = new Size(bitmap.Width, bitmap.Height);
+            }
+
+            coverStream.Position = 0;
+            return new Cover(name, size, coverStream.Length, Observable.Return(coverStream));
+        }
+
+        /// <summary>
+        /// Gets cover from file if exists.
+        /// </summary>
+        /// <param name="tag">The <see cref="Tag"/> of audio.</param>
+        /// <param name="pictureType">The type of picture.</param>
+        /// <returns>The <see cref="Picture"/>.</returns>
+        /// <remarks>If tag contains single image and this image not <see cref="PictureType.FrontCover"/>, will be returned this image.</remarks>
+        public static IPicture GetCoverSafe(this Tag tag, PictureType pictureType)
+        {
+            IPicture picture = tag.Pictures.SingleOrDefault(x => x.Type == pictureType);
+            if (picture == null && tag.Pictures.Any())
+            {
+                picture = tag.Pictures.First();
+            }
+
+            return picture;
+        }
+    }
 }
