1c2cedfafe9a55b9a7d3b0d4493be2f16d5e15ff
Anton Dimkov <ddt.sterh@gmail.com>
2011-10-03 22:56:06 +0300

    Merged style. Updated version to 0.7
    Signed-off-by: Anton Dimkov <ddt.sterh@gmail.com>

--- CoverRetriever/Properties/AssemblyInfo.cs
+++ CoverRetriever/Properties/AssemblyInfo.cs
@@ -40,5 +40,5 @@ using System.Windows;
                              //(used if a resource is not found in the page, 
                              // app, or any theme specific resource dictionaries)
 )]
-[assembly: AssemblyVersionAttribute("0.6.1.*")]
+[assembly: AssemblyVersionAttribute("0.7.0.*")]
 [assembly: NeutralResourcesLanguageAttribute("")]
