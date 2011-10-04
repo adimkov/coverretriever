13178e79d49b8351824452f9b91ef9362a0b6a89
Anton Dimkov <ddt.sterh@gmail.com>
2011-10-04 23:33:22 +0300

    Fixed DirectoryCoverOrganizerTest
    Signed-off-by: Anton Dimkov <ddt.sterh@gmail.com>

--- CoverRetriever.Test/Service/DirectoryCoverOrganizerTest.cs
+++ CoverRetriever.Test/Service/DirectoryCoverOrganizerTest.cs
@@ -7,57 +7,63 @@ using NUnit.Framework;
 
 namespace CoverRetriever.Test.Service
 {
-	[TestFixture]
-	public class DirectoryCoverOrganizerTest
-	{
-		private const string PathToFolderWithoutCover = @"g:\Музыка\ДДТ\[2006] - Family CD2\";
-		private const string PathToFolderWithCover = @"g:\Музыка\ДДТ\[1991] - Пластун\";
+    [TestFixture]
+    public class DirectoryCoverOrganizerTest
+    {
+        private readonly string _pathToFolderWithoutCover;
+        private readonly string _pathToFolderWithCover;
 
-		[Test]
-		public void IsCoverExists_should_return_true()
-		{
-			var target = new DirectoryCoverOrganizer(PathToFolderWithCover);
-			
-			Assert.IsTrue(target.IsCoverExists());
-			Assert.That(target.CoverName, Is.EqualTo("cover.jpg"));
-		}
+        public DirectoryCoverOrganizerTest()
+        {
+            _pathToFolderWithoutCover = PathUtils.BuildFullResourcePath(@"Empty\");
+            _pathToFolderWithCover = PathUtils.BuildFullResourcePath(String.Empty);
+        }
 
-		[Test]
-		public void IsCoverExists_should_return_false()
-		{
-			var target = new DirectoryCoverOrganizer(PathToFolderWithoutCover);
-			
-			Assert.IsFalse(target.IsCoverExists());
-		}
+        [Test]
+        public void IsCoverExists_should_return_true()
+        {
+            var target = new DirectoryCoverOrganizer(_pathToFolderWithCover);
+            
+            Assert.IsTrue(target.IsCoverExists());
+            Assert.That(target.CoverName, Is.EqualTo("cover.jpg"));
+        }
 
-		[Test]
-		public void GetCoverFullPath_should_return_cofer_stream()
-		{
-			var target = new DirectoryCoverOrganizer(PathToFolderWithCover);
-			
-			var cover = target.GetCover();
-			Assert.That(cover, Is.Not.Null);
-			Assert.That(cover.Name, Is.EqualTo("cover.jpg"));
-			Assert.That(cover.CoverStream, Is.Not.Null);
-			Assert.That(cover.Length, Is.EqualTo(56395));
-			Assert.That(cover.CoverSize.Width, Is.EqualTo(497));
-			Assert.That(cover.CoverSize.Height, Is.EqualTo(500));		
-		}
+        [Test]
+        public void IsCoverExists_should_return_false()
+        {
+            var target = new DirectoryCoverOrganizer(_pathToFolderWithoutCover);
+            
+            Assert.IsFalse(target.IsCoverExists());
+        }
 
-		[Test]
-		public void GetCoverStream_should_throw_exception()
-		{
-			var target = new DirectoryCoverOrganizer(PathToFolderWithoutCover);
-			
-			Assert.Throws<InvalidOperationException>(() => target.GetCover());
-		}
+        [Test]
+        public void GetCoverFullPath_should_return_cofer_stream()
+        {
+            var target = new DirectoryCoverOrganizer(_pathToFolderWithCover);
+            
+            var cover = target.GetCover();
+            Assert.That(cover, Is.Not.Null);
+            Assert.That(cover.Name, Is.EqualTo("cover.jpg"));
+            Assert.That(cover.CoverStream, Is.Not.Null);
+            Assert.That(cover.Length, Is.EqualTo(66183));
+            Assert.That(cover.CoverSize.Width, Is.EqualTo(426));
+            Assert.That(cover.CoverSize.Height, Is.EqualTo(425));		
+        }
 
-		[Test]
-		public void Should_check_for_ability_work_with_cover_in_directory_return_true()
-		{
-			var target = new DirectoryCoverOrganizer(PathToFolderWithCover);
-			
-			Assert.IsTrue(target.IsCanProcessed);
-		}
-	}
+        [Test]
+        public void GetCoverStream_should_throw_exception()
+        {
+            var target = new DirectoryCoverOrganizer(_pathToFolderWithoutCover);
+            
+            Assert.Throws<InvalidOperationException>(() => target.GetCover());
+        }
+
+        [Test]
+        public void Should_check_for_ability_work_with_cover_in_directory_return_true()
+        {
+            var target = new DirectoryCoverOrganizer(_pathToFolderWithCover);
+            
+            Assert.IsTrue(target.IsCanProcessed);
+        }
+    }
 }
