4732677e7937b16d9c6316ce9a9d0ebcd842df0f
Anton Dimkov <ddt.sterh@gmail.com>
2012-11-04 12:58:49 +0200

    Disable FileEditer if nothing selected
    Signed-off-by: Anton Dimkov <ddt.sterh@gmail.com>

--- CoverRetriever/Converter/NullToVisibilityConverter.cs
+++ CoverRetriever/Converter/NullToVisibilityConverter.cs
@@ -1,8 +1,17 @@
-using System.Windows;
+// -----------------------------------------------------------------------------------------------
+// <copyright file="NullToVisibilityConverter.cs" author="Anton Dimkov">
+//   Copyright (c) Anton Dimkov 2011. All rights reserved.  
+// </copyright>
+// --------------------------------------------------------------------------------------------
+
 namespace CoverRetriever.Converter
 {
-	public class NullToVisibilityConverter : NullToBoolConverter<Visibility>
-	{
-		
-	}
+    using System.Windows;
+
+    /// <summary>
+    /// Declaration of the <see cref="NullToVisibilityConverter" /> converter.
+    /// </summary>
+    public class NullToVisibilityConverter : NullToRecipientConverter<Visibility>
+    {
+    }
 }
